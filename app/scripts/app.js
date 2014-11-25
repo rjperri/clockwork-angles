56ecdc59a992d03bce3a8777e18fb6e18ea001ef
Bob Perri <perrirj68@gmail.com>
2014-11-25 00:19:36 -0500

    1st upload of project

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name clockworkAnglesApp
+ * @description
+ * # clockworkAnglesApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('clockworkAnglesApp', [
+    'ngResource'
+  ]);
